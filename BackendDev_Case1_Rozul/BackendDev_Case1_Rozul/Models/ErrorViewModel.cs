89bc71ed6085f2b7a6cd192af8c977e508843404
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-05-04 16:24:53 +0800

    misc: Project Initialization (Case 1)
    Project
    - BackendDev_Case1
    
    Added Packages
    - Microsoft.EntityFrameworkCore
    - Microsoft.EntityFrameworkCore.Design
    - Microsoft.EntityFrameworkCore.SqlServer
    - Microsoft.EntityFrameworkCore.Tools

---
+++ BackendDev_Case1_Rozul/BackendDev_Case1_Rozul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackendDev_Case1_Rozul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
