6a935a3b11275895fc3889eacd7aa8c4332b1d5c
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-05-04 23:31:02 +0800

    feat: Order Controller
    CRUD Operations
    - Get Orders List
    - Get Order by ID
    - Create Order
    - Update Order
    - Delete Order

--- BackendDev_Case1_Rozul/BackendDev_Case1_Rozul/Entities/Order.cs
+++ BackendDev_Case1_Rozul/BackendDev_Case1_Rozul/Entities/Order.cs
@@ -14,6 +14,7 @@ namespace BackendDev_Case1_Rozul.Entities{
 
         public DateTime OrderDate { get; set; } = DateTime.Now;
         
+        public bool IsDeleted { get; set; } = false;
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 
@@ -22,15 +23,19 @@ namespace BackendDev_Case1_Rozul.Entities{
         public int OrderID { get; set;}
         public string? CustomerName {get; set;}
         public DateTime OrderDate {get; set;}
+        public decimal OrderTotalPrice { get; set; } = 0;
         public List<OrderItemDTO> Items { get; set; } = new();
+        public bool IsDeleted { get; set; }
         public OrderDTO(Order order){
             OrderID = order.OrderID;
             CustomerName = order.CustomerName;
             OrderDate = order.OrderDate;
-
+            IsDeleted = order.IsDeleted;
             // Convert all order item to OrderDTO
             foreach(OrderItem orderItem in order.OrderItems){
-                Items.Add(new OrderItemDTO(orderItem));
+                OrderItemDTO orderItemDTO = new OrderItemDTO(orderItem);
+                OrderTotalPrice += orderItemDTO.UnitPrice;
+                Items.Add(orderItemDTO);
             }
         }
     }
